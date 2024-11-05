f4b1f47ad8b987987629e34202c72ed842a2a18b
Boateng Prince Agyenim <163312213+Mmabiaa@users.noreply.github.com>
2024-11-05 14:41:37 +0000

    Add files via upload
    This a simple python program that sends messages automatically to a number

---
+++ Python_Begginer_Projects/Intermediate/TextMessage_Automation.py
@@ -0,0 +1,17 @@
+import requests
+import schedule
+import time
+
+phone_number = '' # Enter the number you're sending the message to. 
+
+def send_message():
+    resp = requests.post('http;//textbelt.com/text', {
+    'phone': phone_number, 
+    'message': 'Hello, world',
+    'key': 'textbelt'
+    })
+
+    print(resp.json())
+# Schedule at 6pm everyday.
+schedule.every().day.at('18:00').do(send_message)
+
