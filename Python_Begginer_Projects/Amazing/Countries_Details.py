a0d9df38c2c86b0847ab633d9d5f3d1cf69b5d6d
Boateng Prince Agyenim <163312213+Mmabiaa@users.noreply.github.com>
2024-11-18 01:09:26 +0000

    Update Countries_Details.py

--- Python_Begginer_Projects/Amazing/Countries_Details.py
+++ Python_Begginer_Projects/Amazing/Countries_Details.py
@@ -1,25 +1,30 @@
 from countryinfo import CountryInfo
-
-count = input('Enter the name of the country or -1 to quit: ')
-
-while count != '-1':
-    country = CountryInfo(count)
-
-    print("Capital is: ", country.capital())
-    print('----------------------------------------------------------------')
-    print('Currency is: ', country.currencies())
-    print('----------------------------------------------------------------')
-    print('Language is: ', country.languages())
-    print('----------------------------------------------------------------')
-    print('Borders are: ', country.borders())
-    print('----------------------------------------------------------------')
-    print('Other names are: ', country.alt_spellings())
-    print('----------------------------------------------------------------')
-    print('Population is: ', country.population())
-    print('----------------------------------------------------------------')
-    print('Time zone is: ',country.timezones())
-    print('----------------------------------------------------------------')
+try:
     count = input('Enter the name of the country or -1 to quit: ')
-    print('----------------------------------------------------------------')
-else:
-    print('Thanks for using the program...')
+    while count != '-1':
+    
+        country = CountryInfo(count)
+
+        print("Capital is: ", country.capital())
+        print('----------------------------------------------------------------')
+        print('Currency is: ', country.currencies())
+        print('----------------------------------------------------------------')
+        print('Language is: ', country.languages())
+        print('----------------------------------------------------------------')
+        print('Borders are: ', country.borders())
+        print('----------------------------------------------------------------')
+        print('Other names are: ', country.alt_spellings())
+        print('----------------------------------------------------------------')
+        print('Population is: ', country.population())
+        print('----------------------------------------------------------------')
+        print('Time zone is: ',country.timezones())
+        print('----------------------------------------------------------------')
+        count = input('Enter the name of the country or -1 to quit: ')
+        print('----------------------------------------------------------------')
+    else:
+        print('Thanks for using the program...')
+except KeyError:
+    print('Country was not found!')
+    print('Try Again with a valid country name.')
+
+
