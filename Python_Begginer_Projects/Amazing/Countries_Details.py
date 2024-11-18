59ae63fb0ad92e16aaacca5c95ee74b067280c17
Boateng Prince Agyenim <163312213+Mmabiaa@users.noreply.github.com>
2024-11-18 01:01:26 +0000

    Add files via upload

---
+++ Python_Begginer_Projects/Amazing/Countries_Details.py
@@ -0,0 +1,25 @@
+from countryinfo import CountryInfo
+
+count = input('Enter the name of the country or -1 to quit: ')
+
+while count != '-1':
+    country = CountryInfo(count)
+
+    print("Capital is: ", country.capital())
+    print('----------------------------------------------------------------')
+    print('Currency is: ', country.currencies())
+    print('----------------------------------------------------------------')
+    print('Language is: ', country.languages())
+    print('----------------------------------------------------------------')
+    print('Borders are: ', country.borders())
+    print('----------------------------------------------------------------')
+    print('Other names are: ', country.alt_spellings())
+    print('----------------------------------------------------------------')
+    print('Population is: ', country.population())
+    print('----------------------------------------------------------------')
+    print('Time zone is: ',country.timezones())
+    print('----------------------------------------------------------------')
+    count = input('Enter the name of the country or -1 to quit: ')
+    print('----------------------------------------------------------------')
+else:
+    print('Thanks for using the program...')
