1a0607b35efc64e1db28e5e876cfae4f94f38b2c
Boateng Prince Agyenim <163312213+Mmabiaa@users.noreply.github.com>
2024-11-20 20:47:57 +0000

    Update World_Map.py

--- Python_Begginer_Projects/Amazing/World_Map.py
+++ Python_Begginer_Projects/Amazing/World_Map.py
@@ -2,7 +2,7 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 
 world = gpd.read_file('Python_Begginer_Projects/Amazing/ne_110m_admin_0_countries.shp')
-
+# main function to display the map
 world.plot(edgecolor='red')
 plt.title('World Map with Country Borders')
 plt.show()
